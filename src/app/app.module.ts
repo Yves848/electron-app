cb4c5115958e2a2f469624111738b2a2685cd92d
yves.godart@gmail.com <Yves848>
2021-05-05 16:54:51 +0200

    ajout des composants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxElectronModule } from 'ngx-electron';
+import { PrimeComponentsModule } from './components/primeComponents';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaincomponentComponent } from './components/maincomponent/maincomponent.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MaincomponentComponent
-  ],
+  declarations: [AppComponent, MaincomponentComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    NgxElectronModule
+    NgxElectronModule,
+    PrimeComponentsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
